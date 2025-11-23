6f31ba3193e9cb13ded06fbb85bac81d91c23f38
zagorky <zagorkiynahyi@gmail.com>
2025-11-23 11:50:25 +0300

    feat: integrate Swagger for API documentation generation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as yaml from 'yamljs';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swaggerDocument = yaml.load('./doc/api.yaml');
+  SwaggerModule.setup('api', app, swaggerDocument);
   await app.listen(process.env.PORT);
 }
 bootstrap();
