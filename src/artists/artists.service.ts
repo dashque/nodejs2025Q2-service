2780b9b8629ac75d7a054766378c184dbc5ba64d
zagorky <zagorkiynahyi@gmail.com>
2025-11-27 15:30:46 +0300

    feat: implement in-memory storage for artists, enhance DTO mappings, and refactor service methods for consistent responses

--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -1,26 +1,66 @@
 import { Injectable } from '@nestjs/common';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
+import { Artist } from './entities/artist.entity';
+import { ArtistResponseDto } from './dto/artists.dto';
+import { checkExistenceOrThrow } from '../utils/utils';
 
 @Injectable()
 export class ArtistsService {
+  private readonly artists = new Map<string, Artist>();
+
+  private mapToResponseDto(artist: Artist): ArtistResponseDto {
+    return {
+      id: artist.id,
+      name: artist.name,
+      grammy: artist.grammy,
+    };
+  }
+
   create(createArtistDto: CreateArtistDto) {
-    return 'This action adds a new artist';
+    const newArtist = new Artist(createArtistDto.name, createArtistDto.grammy);
+    this.artists.set(newArtist.id, newArtist);
+    return this.mapToResponseDto(newArtist);
   }
 
   findAll() {
-    return `This action returns all artists`;
+    return Array.from(this.artists.values()).map(this.mapToResponseDto);
   }
 
   findOne(id: string) {
-    return `This action returns a #${id} artist`;
+    const artist = checkExistenceOrThrow({
+      id,
+      map: this.artists,
+      name: 'Artist',
+    });
+
+    return this.mapToResponseDto(artist);
   }
 
   update(id: string, updateArtistDto: UpdateArtistDto) {
-    return `This action updates a #${id} artist`;
+    const artist = checkExistenceOrThrow({
+      id,
+      map: this.artists,
+      name: 'Artist',
+    });
+
+    if (updateArtistDto.name !== undefined) {
+      artist.name = updateArtistDto.name;
+    }
+    if (updateArtistDto.grammy !== undefined) {
+      artist.grammy = updateArtistDto.grammy;
+    }
+
+    this.artists.set(id, artist);
+    return this.mapToResponseDto(artist);
   }
 
   remove(id: string) {
-    return `This action removes a #${id} artist`;
+    checkExistenceOrThrow({
+      id,
+      map: this.artists,
+      name: 'Artist',
+    });
+    this.artists.delete(id);
   }
 }
