43c1eeb8c7e565dc4605a6ea0e658b11d51ec77c
zagorky <zagorkiynahyi@gmail.com>
2025-11-27 16:36:24 +0300

    feat: initialize arrays in Favorite entity constructor, import related modules in FavoritesModule

--- src/favorites/entities/favorite.entity.ts
+++ src/favorites/entities/favorite.entity.ts
@@ -1,13 +1,11 @@
-// interface Favorites {
-//   artists: string[]; // favorite artists ids
-//   albums: string[]; // favorite albums ids
-//   tracks: string[]; // favorite tracks ids
-// }
-
 export class Favorite {
   artists: string[];
   albums: string[];
   tracks: string[];
 
-  constructor() {}
+  constructor() {
+    this.artists = [];
+    this.albums = [];
+    this.tracks = [];
+  }
 }
--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { FavoritesController } from './favorites.controller';
+import { ArtistsModule } from '../artists/artists.module';
+import { AlbumsModule } from '../albums/albums.module';
+import { TracksModule } from '../tracks/tracks.module';
 
 @Module({
   controllers: [FavoritesController],
   providers: [FavoritesService],
+  imports: [TracksModule, ArtistsModule, AlbumsModule],
 })
 export class FavoritesModule {}
