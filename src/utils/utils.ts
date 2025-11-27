011dd761441db01208d05a3cf095e2fb36ac1832
zagorky <zagorkiynahyi@gmail.com>
2025-11-27 21:41:46 +0300

    feat: add utility functions for handling favorites, enhance ID validation and error handling

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,13 @@
-import { NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { validate } from 'uuid';
+import { Favorite } from '../favorites/entities/favorite.entity';
+import { Track } from '../tracks/entities/track.entity';
+import { Album } from '../albums/entities/album.entity';
+import { Artist } from '../artists/entities/artist.entity';
 
 export const checkExistenceOrThrow = <T>({
   id,
@@ -14,4 +23,57 @@ export const checkExistenceOrThrow = <T>({
     throw new NotFoundException(`${name} not found`);
   }
   return elem;
+};
+
+export const addElementToFavs = <
+  K extends (Track | Album | Artist)[],
+  T extends Track | Album | Artist,
+>({
+  id,
+  data,
+  name,
+  rawData,
+}: {
+  id: string;
+  data: K;
+  name: string;
+  rawData: Map<PropertyKey, T>;
+}) => {
+  if (!validate(id)) {
+    throw new BadRequestException(`Invalid ${name} format`);
+  }
+
+  const existingTrack = data.find((elem) => elem.id === id);
+  if (existingTrack) {
+    throw new UnprocessableEntityException(`${name} already in favorites`);
+  }
+
+  const elem = rawData.get(id);
+  if (!elem) {
+    throw new UnprocessableEntityException(`${name} not found`);
+  }
+
+  data.push(elem);
+  return elem;
+};
+
+export const filterData = <
+  K extends Favorite,
+  T extends Track | Album | Artist,
+>({
+  id,
+  data,
+  name,
+}: {
+  id: string;
+  data: K;
+  name: string;
+}) => {
+  const initialLength = data[name].length;
+  const filteredData = data[name].filter((elem: T) => elem.id !== id);
+  if (data[name].length === initialLength) {
+    throw new Error(`${name} not found in favorites`);
+  }
+  data[name] = filteredData;
+  return filteredData;
 };
